a149535673c57ba44744d5b57c60e57e2ac7364c
Javan Oyugi <jooyugi@safaricom.co.ke>
2023-06-26 14:15:04 +0300

    use scoped css for header and footer

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -1,123 +1,124 @@
 import React from "react";
 import logo from './image.svg';
-import './footer.css';
+import styles from './Footer.module.css';
+import headerStyles from "./Nav.module.css"
 const Footer = () => {
     return (<>
-        <div className="footer">
-            <div className="footer-columns">
-                <div className="footer-column">
-                    <div className="footer-header">
+        <div className={styles.footer}>
+            <div className={styles.footerColumns}>
+                <div className={styles.footerColumn}>
+                    <div className={styles.footerHeader}>
                         Company
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         About
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Affiliate
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Careers & Culture
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Blog
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Press
                     </div>
                 </div>
-                <div className="footer-column">
-                    <div className="footer-header">
+                <div className={styles.footerColumn}>
+                    <div className={styles.footerHeader}>
                         About Us
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Support Center
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Customer Support
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         About Us
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Copyright
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Popular Campaign
                     </div>
                 </div>
-                <div className="footer-column">
-                    <div className="footer-header">
+                <div className={styles.footerColumn}>
+                    <div className={styles.footerHeader}>
                         Our Information
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Return Policy
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Privacy Policy
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Terms & Conditions
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Site Map
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Store Hours
                     </div>
                 </div>
-                <div className="footer-column">
-                    <div className="footer-header">
+                <div className={styles.footerColumn}>
+                    <div className={styles.footerHeader}>
                         My Account
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Press Inquiries
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Social Media
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Directories
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Images & B-roll
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Permissions
                     </div>
                 </div>
-                <div className="footer-column">
-                    <div className="footer-header">
+                <div className={styles.footerColumn}>
+                    <div className={styles.footerHeader}>
                         Policy
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Application Security
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Software Principles
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Unwanted Software Policy
                     </div>
-                    <div className="footer-link">
+                    <div className={styles.footerLink}>
                         Responsible Supply Chain
                     </div>
                 </div>
             </div>
-            <div className="footer-hr"></div>
-            <div className="footer-columns">
-                <div className="home-link">
-                    <img src={logo} alt="Logo" class="logo" />
+            <div className={styles.footerHr}></div>
+            <div className={styles.footerColumns}>
+                <div className={headerStyles.homeLink}>
+                    <img src={logo} alt="Logo" className={headerStyles.logo} />
                     ChainLink
                 </div>
-                <div className="copyright">
+                <div className={styles.copyright}>
                     Copyright {new Date().getFullYear()}
                 </div>
-                <div className="bottom-footer-links">
-                    <div className="bottom-footer-link">Home</div>
-                    <div className="bottom-footer-link">Advertise</div>
-                    <div className="bottom-footer-link">Support</div>
-                    <div className="bottom-footer-link">Marketing</div>
-                    <div className="bottom-footer-link">FAQ</div>
+                <div className={styles.bottomFooterLinks}>
+                    <div className={styles.bottomFooterLink}>Home</div>
+                    <div className={styles.bottomFooterLink}>Advertise</div>
+                    <div className={styles.bottomFooterLink}>Support</div>
+                    <div className={styles.bottomFooterLink}>Marketing</div>
+                    <div className={styles.bottomFooterLink}>FAQ</div>
                 </div>
             </div>
         </div>
