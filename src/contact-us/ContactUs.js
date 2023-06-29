bc3a2e3e1080de24b32163fce299529a318bf1ed
Javan Oyugi <jooyugi@safaricom.co.ke>
2023-06-29 11:10:26 +0300

    use scoped css

--- src/contact-us/ContactUs.js
+++ src/contact-us/ContactUs.js
@@ -6,16 +6,18 @@ import Location from './location/Location';
 import Footer from '../common/Footer';
 import Nav from '../common/Nav';
 
+import styles from "./ContactUs.module.css"
+
 
 const ContactUs = () => {
     return (
-        <>
+        <div className={styles.contactUs}>
             <Nav navLinkItem="contact"/>
             <TopSection />
             <MidSection />
             <Location/>
             <Footer/>
-        </>
+        </div>
     )
 };
 export default ContactUs;
