12e3a5cd093d36a81fe469b885681111b545f130
pbogonko <pbogonko@safaricom.co.ke>
2023-06-29 09:55:32 +0300

    more changes made to our solutions page

--- src/our-solutions/bottom-section/BottomSection.js
+++ src/our-solutions/bottom-section/BottomSection.js
@@ -1,43 +1,64 @@
 import React from 'react';
 import "./BottomSection.css";
+import solution from "../theSolution.jpg"
+import benefit1 from "../pic.jpg"
+import benefit2 from "../pic5.jpg"
+import benefit3 from "../pic2.jpg"
 
 const BottomSection = ({ props }) => {
     return (
-        <div className="solutions-page">
-            <section>
-                <div className="container mx-auto py-5">
-                    <div className="mb-4 text-center">
+        <>
+            <div className="container mx-auto py-5  ">
+                    <div className="text-center pt-3">
                         <div className="row-header">
                             Meet the Exciting Features of our Solution
                             <div className='underline'></div>
                         </div>
                     </div>
-                    <div className="row gy-5 d-flex align-items-center justify-content-evenly mt-0">
+                    <div className="row gy-5 d-flex align-items-center justify-content-evenly mt-0 myRow">
                         <div className="col-md-4 col-lg-3 text-center">
-                            <div className="box_border position-relative rounded pt-5 pb-3 px-3">
+                            <div className="box_border position-relative rounded pt-5 pb-3 px-3 borderBottom shadow">
                                 <div className="shp"></div>
-                                <p className="mb-2 mt-4">Brand Building</p>
-                                <p className="text-muted text-left text-xl-center text-lg-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                <p className="mb-2 mt-4">All in One Tracker</p>
+                                <p className="text-muted text-left text-xl-center text-lg-center">Track your business income and expenditure real time either from legacy systems or mpesa or bank.</p>
                             </div>
                         </div>
                         <div className="col-md-4 col-lg-3 text-center">
-                            <div className="box_border position-relative rounded pt-5 pb-3 px-3">
+                            <div className="box_border position-relative rounded pt-5 pb-3 px-3 borderBottom shadow">
                                 <div className="shp"></div>
-                                <p className="mb-2 mt-4">Pixel Perfect Designing</p>
-                                <p className="text-muted text-left text-xl-center text-lg-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                <p className="mb-2 mt-4">Real-time Dashboards</p>
+                                <p className="text-muted text-left text-xl-center text-lg-center">Avoid money loss  when transacting with different accounts by having a common real-time view</p>
                             </div>
                         </div>
                         <div className="col-md-4 col-lg-3 text-center">
-                            <div className="box_border position-relative rounded pt-5 pb-3 px-3">
+                            <div className="box_border position-relative rounded pt-5 pb-3 px-3 borderBottom shadow">
                                 <div className="shp"></div>
-                                <p className="mb-2 mt-4">Marketing</p>
-                                <p className="text-muted text-left text-xl-center text-lg-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                <p className="mb-2 mt-4">Multiple interfaces</p>
+                                <p className="text-muted text-left text-xl-center text-lg-center">Track your sales , debts and expenditures with ease from both mobile and web tools</p>
                             </div>
+                        </div>
+                    </div>
+
+
+        </div>
+
+
+
+            <section className="sec_2 flex">
+                <div className="sec_bg py-5">
+                    <div className="container d-flex justify-content-center align-items-center ">
+                        <div className=" col-sm-9 col-sm-6 text-center py-3">
+                            <p className="display-6 mb-1 text-white">Automatic tracker for your expenditure, debts and sales</p>
+                            <img className="image-fluid py-3" alt="brand" width="40%" src={solution} />
+                            <p className="text-white lead">Your sales supply chain link</p>
                         </div>
                     </div>
                 </div>
             </section>
-        </div>
+
+
+
+        </>
 
 
     )
--- src/our-solutions/mid-section/MidSection.js
+++ src/our-solutions/mid-section/MidSection.js
@@ -5,8 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
  const Scards = (props) => {
      return (
-         <div className="col-md-4 col-lg-4 mb-3 text-center">
-             <div className="box_border col-lg-9 p-3 pt-4 m-auto rounded">
+         <div className="col-md-4 col-lg-4 mb-3 text-center  ">
+             <div className="box_border col-lg-9 p-3 pt-4 m-auto rounded border-data shadow">
                  <img src={props.scardimage} className="img-fluid mb-2" alt="cards" width="20%" />
                  <p className="my-2">{props.scardtitle}</p>
                  <p className="text-muted text-left text-xl-center text-lg-center">{props.scarddesc}</p>
@@ -16,16 +16,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
  };
 
 const MidSection = () => {
+    const rowStyle = {
+        backgroundColor: 'transparent',
+    };
+
     return (
         <>
 
             <section>
-                <div className="container mx-auto py-5">
+            <div className="container mx-auto py-5">
                     <div className="mb-5 text-center">
                         <p className="display-6 mb-1 row-header">Ultimate Features in one dashboard</p>
                         <div className='underline'></div>
                     </div>
-                    <div className="row d-flex items-align-center justify-content-evenly">
+                    <div className="row d-sm-flex items-align-center justify-content-between myRow" style={rowStyle}>
                         {
                             Data.map((val, ind) => {
 
