eaa6d71162841286218fd74548c9dfd99df87ebb
Javan Oyugi <jooyugi@safaricom.co.ke>
2023-07-02 22:14:34 +0300

    add login screen

---
+++ src/login/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from '../common/Nav';
+import Footer from "../common/Footer";
+
+const Login = ({ props }) => {
+  return (
+    <>
+      <Nav navLinkItem={"login"} />
+      <div>
+        Coming soon . . .
+      </div>
+      <Footer/>
+    </>
+  );
+};
+
+export default Login;
