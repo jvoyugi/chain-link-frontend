44ca280220bf82b91ed8ae82adf2f7cf83d86a05
Javan Oyugi <jooyugi@safaricom.co.ke>
2023-07-27 22:15:51 +0300

    inventory

---
+++ src/portal/inventory/Inventory.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TableDetails from "./TableDetails";
+import InputPopUp from "./components/InputPopUp";
+import PopUp from "./components/PopUp";
+import styles from "./Inventory.module.css";
+import Layout from '../../common/Layout';
+
+const Inventory = () => {
+    const [buttonPopUp, setButtonPopUp] = useState(false);
+    const [businesses, setBusinesses] = useState([]);
+
+    useEffect(() => {
+        const fetchBusinesses = async () => {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/api/business`,
+                {
+                    mode: "cors",
+                    method: "GET",
+                    credentials: "include"
+                })
+            if (response.ok) {
+                setBusinesses(await response.json())
+            }
+        }
+        (async () => await fetchBusinesses())();
+        return () => { }
+    }, []);
+
+    return (<Layout pageTitle={"Inventory"} navLinkItem={"inventory"} child={
+        <>
+            <div>
+                <div className="row align-items-center justify-content-end " className={styles.popUp} >
+                    <button className="col-md-9" className={styles.manualButton} onClick={() => setButtonPopUp(true)} >Add item</button>
+                    <PopUp trigger={buttonPopUp} setTrigger={setButtonPopUp} >
+                        <InputPopUp setTrigger={setButtonPopUp} businesses={businesses}/>
+                    </PopUp>
+                </div>
+                <div className=" row justify-content-right pt-5">
+                    <div className="text-right text-success font-weight-bold" className={styles.dashboardTitle}>
+                        <h2 className="card-title">Inventory</h2>
+                    </div>
+                </div>
+                <div className="card-content">
+                    <TableDetails />
+                </div>
+            </div>
+        </>} />
+    )
+};
+export default Inventory;
---
+++ src/portal/inventory/TableDetails.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from "./TableDetails.module.css";
+import DeletePopUp from "./components/DeletePopUp";
+import EditPopUp from "./components/EditPopUp";
+import PopUp from "./components/PopUp";
+import { BsFillPencilFill, BsFillTrashFill } from "react-icons/bs";
+
+const TableDetails = () => {
+    const [data, setData] = useState([]);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [editPopUp, setEditPopUp] = useState(false);
+    const [deletePopUp, setDeletePopUp] = useState(false);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            let resp = await fetch(`${process.env.REACT_APP_API_URL}/api/inventory`,
+                {
+                    mode: "cors",
+                    method: "GET",
+                    credentials: "include"
+                });
+            if (resp.ok) {
+                setData(await resp.json());
+            }
+        }
+        let timerId = setTimeout(fetchData, 2000);
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [data])
+
+    return (
+        <div className="row">
+            <div className="table-responsive">
+                <table className="table table-bordered table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Business</th>
+                            <th>Quantity</th>
+                            <th>Cost</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody className="flex align-items-center justify-content-center" >
+                        {data.length > 0 && data.map(item => (
+                            <tr key={item._id} className="p-1 align-items-center justify-content-center">
+                                <td>{item.name}</td>
+                                <td>{item.businessName.businessName}</td>
+                                <td>{item.quantity}</td>
+                                <td>{item.cost * item.quantity}</td>
+                                <td className={styles.tableActions} >
+                                    <span className="flex justify-content-around">
+                                        <BsFillPencilFill color="red" className="m-2" onClick={() => { setEditPopUp(true); setSelectedItem(item) }} />
+                                        <BsFillTrashFill color="red" className="m-2" onClick={() => { setDeletePopUp(true); setSelectedItem(item) }} />
+                                    </span>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <div>
+                    <PopUp trigger={editPopUp} setTrigger={setEditPopUp} >
+                        <EditPopUp setTrigger={setEditPopUp} selectedItem={selectedItem} />
+                    </PopUp>
+
+                    <PopUp trigger={deletePopUp} setTrigger={setDeletePopUp} >
+                        <DeletePopUp setTrigger={setDeletePopUp} selectedItem={selectedItem} />
+                    </PopUp>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TableDetails;
+
